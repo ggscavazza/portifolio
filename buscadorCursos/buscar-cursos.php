924e12916dd7f446cd202a8c65a2b17d7977dc9b
Gustavo G. Scavazza <40220303+ggscavazza@users.noreply.github.com>
2023-01-16 16:11:27 -0300

    Create buscar-cursos.php

---
+++ buscadorCursos/buscar-cursos.php
@@ -0,0 +1,17 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Genesis\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['base_uri' => 'https://www.alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
